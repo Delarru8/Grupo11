e10349e709c904bb6c8e6ecf22c5f2aeecf1c216
abanicadordepavos <37449961+abanicadordepavos@users.noreply.github.com>
2018-04-30 17:54:49 +0200

    guardados libros

--- MyApp/src/pages/home/home.ts
+++ MyApp/src/pages/home/home.ts
@@ -56,26 +56,55 @@ export class HomePage {
   
   addLibro(){
 	let datoslibro5:Libro=new Libro();
-	datoslibro5.titulo="Harrypopop Potter";
+	datoslibro5.titulo="Harry Potter";
 	datoslibro5.autor="J. K. Rowling";
 	datoslibro5.imagen="https://firebasestorage.googleapis.com/v0/b/siers-1222a.appspot.com/o/libro.jpg?alt=media&token=e42170a1-ae3c-4d28-aa3e-87cddb521177";
 	this.dbFirebase.guardarLibro(datoslibro5).then(res=>{alert(datoslibro5.titulo+ " guardado en FB");});
 	let datoslibro:Libro=new Libro();
-	datoslibro.titulo="Harry Potter";
-	datoslibro.autor="J. K. Rowling";
+	datoslibro.titulo="El Señor de los Anillos";
+	datoslibro.autor=" J.R.R. Tolkien";
+	datoslibro.imagen="https://firebasestorage.googleapis.com/v0/b/siers-1222a.appspot.com/o/9788445076118.jpg?alt=media&token=03615e82-ecf1-4af8-8227-8c54fe741fe4";
 	this.dbFirebase.guardarLibro(datoslibro).then(res=>{alert(datoslibro.titulo+ " guardado en FB");});
 	let datoslibro2:Libro=new Libro();
 	datoslibro2.titulo="Los Juegos Del Hambre";
-	datoslibro2.autor="Persona Famosa";
+	datoslibro2.autor="Suzanne Collins";
+	datoslibro2.imagen="https://firebasestorage.googleapis.com/v0/b/siers-1222a.appspot.com/o/9780439023481.jpg?alt=media&token=e5b74379-5005-4bdf-9eae-532b624f187e";
 	this.dbFirebase.guardarLibro(datoslibro2).then(res=>{alert(datoslibro2.titulo+ " guardado en FB");});
 	let datoslibro3:Libro=new Libro();
 	datoslibro3.titulo="Diario de Greg";
-	datoslibro3.autor="Yo";
+	datoslibro3.autor="Jeff Kinney";
+	datoslibro3.imagen="https://firebasestorage.googleapis.com/v0/b/siers-1222a.appspot.com/o/9788498672220.jpg?alt=media&token=4419cecd-21a2-4b69-82e1-8c20b7de12cc";
 	this.dbFirebase.guardarLibro(datoslibro3).then(res=>{alert(datoslibro3.titulo+ " guardado en FB");});
 	let datoslibro4:Libro=new Libro();
-	datoslibro4.titulo="Me llamo Ralph";
-	datoslibro4.autor="Matt Groening";
+	datoslibro4.titulo="Eragon";
+	datoslibro4.autor="Stefen Fangmeier";
+	datoslibro4.imagen="https://firebasestorage.googleapis.com/v0/b/siers-1222a.appspot.com/o/9788496940581.jpg?alt=media&token=babc2815-1fde-4e24-b52e-0c8a23c147cc";
 	this.dbFirebase.guardarLibro(datoslibro4).then(res=>{alert(datoslibro4.titulo+ " guardado en FB");});
+	let datoslibro6:Libro=new Libro();
+	datoslibro6.titulo="Memorias de Idhún";
+	datoslibro6.autor="Laura Gallego";
+	datoslibro6.imagen="https://firebasestorage.googleapis.com/v0/b/siers-1222a.appspot.com/o/9788467502695.jpg?alt=media&token=119ac767-fe95-4f39-bde6-b3fb3e25d6b5";
+	this.dbFirebase.guardarLibro(datoslibro6).then(res=>{alert(datoslibro6.titulo+ " guardado en FB");});
+	let datoslibro7:Libro=new Libro();
+	datoslibro7.titulo="El barco de la muerte";
+	datoslibro7.autor="William Clark Russell";
+	datoslibro7.imagen="https://firebasestorage.googleapis.com/v0/b/siers-1222a.appspot.com/o/WCRussell.jpg?alt=media&token=91cbcd43-ac4b-4cc4-9f41-776d1544089e";
+	this.dbFirebase.guardarLibro(datoslibro7).then(res=>{alert(datoslibro7.titulo+ " guardado en FB");});
+	let datoslibro8:Libro=new Libro();
+	datoslibro8.titulo="La guerra de los mundos";
+	datoslibro8.autor="H. G. Wells";
+	datoslibro8.imagen="https://firebasestorage.googleapis.com/v0/b/siers-1222a.appspot.com/o/wmg_art_10579_2961.jpg?alt=media&token=2cd8ec0e-934d-499d-ae08-9476e4da8eba";
+	this.dbFirebase.guardarLibro(datoslibro8).then(res=>{alert(datoslibro8.titulo+ " guardado en FB");});
+	let datoslibro9:Libro=new Libro();
+	datoslibro9.titulo="El ocho";
+	datoslibro9.autor="Katherine Neville";
+	datoslibro9.imagen="https://firebasestorage.googleapis.com/v0/b/siers-1222a.appspot.com/o/9788483465202.jpg?alt=media&token=84766bda-fcf1-4ac7-9841-aa99a22872ff";
+	this.dbFirebase.guardarLibro(datoslibro9).then(res=>{alert(datoslibro8.titulo+ " guardado en FB");});
+	let datoslibro10:Libro=new Libro();
+	datoslibro10.titulo="Y no quedó ninguno";
+	datoslibro10.autor="Agatha Christie";
+	datoslibro10.imagen="https://firebasestorage.googleapis.com/v0/b/siers-1222a.appspot.com/o/image_1165_1_96565.jpg?alt=media&token=bedb0247-b2e8-4fe1-a148-181c8b16d303";
+	this.dbFirebase.guardarLibro(datoslibro10).then(res=>{alert(datoslibro10.titulo+ " guardado en FB");});
  }
   
  delLibro(titulo) { this.dbFirebase.delLibro(titulo); }
