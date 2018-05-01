2f7e00b4798ff244209958a2ceeafe1bb717d342
Disagon <dipum24@gmail.com>
2018-05-01 23:04:32 +0200

    Todo perfectoa salvo cerrar sesión

--- MyApp/src/pages/espera/espera.ts
+++ MyApp/src/pages/espera/espera.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import {SiersProvider} from '../../providers/siers/siers';
-import {Pedido} from '../../models/pedido.model';
 import { AlertController } from 'ionic-angular';
 
 /**
@@ -19,8 +18,6 @@ import { AlertController } from 'ionic-angular';
 })
 export class EsperaPage {
 	public listaPedidos:any[];
-	public libros:any[];
-	public librosBuenos:String[];
 	public newuser:any;
 	public unLibro: any;
 	public unUser: any;
@@ -28,36 +25,11 @@ export class EsperaPage {
 	
   constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public dbFirebase:SiersProvider) {
 	  this.newuser = navParams.get("unUser");
-	  this.dbFirebase.getPedidos().subscribe(listaPedidos=>{this.listaPedidos=listaPedidos;
-		var _libros: String[] = new Array(this.listaPedidos.length);
-        for(var libro in this.listaPedidos){
-            _libros[libro] = this.listaPedidos[libro].nombre;
-        }
-		this.librosBuenos = _libros.filter((libro) => {
-                return (libro.toLowerCase().indexOf(this.newuser.nombre.toLowerCase()) > -1);
-        });
-		this.libros = new Array(this.librosBuenos.length);
-		var k: any;
-		var j: number = 0;
-		for(k in this.listaPedidos){
-			if(this.listaPedidos[k].nombre == this.newuser.nombre){
-				this.libros[j]=this.listaPedidos[k];
-				j = j+1;
-				alert(this.libros[j].titulo);
-			}
-		}
-	  });
+	  this.dbFirebase.getPedidos().subscribe(listaPedidos=>{this.listaPedidos=listaPedidos;});
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PedidosPage');
-  }
-
-  prep(pedido:Pedido){
-	  if(pedido.preparado == "no"){
-		  return false;
-	  }
-	  return true;
   }
   
   perfil(){
