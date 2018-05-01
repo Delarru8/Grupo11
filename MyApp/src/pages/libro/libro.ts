ec88f0529e969b343dd6fc6f2957f910d5c20284
Disagon <dipum24@gmail.com>
2018-05-01 12:56:39 +0200

    Revert "Merge branch 'master' of https://github.com/Delarru8/Grupo11"
    This reverts commit 3268caceecd3a55f044ab2ca51358d9ad549eca2, reversing
    changes made to 7b20a8961f4cc174b7c95ca63d3a57d3becf8328.

--- MyApp/src/pages/libro/libro.ts
+++ MyApp/src/pages/libro/libro.ts
@@ -3,7 +3,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import {SiersProvider} from '../../providers/siers/siers';
 import {Libro} from '../../models/libro.model';
-import { AlertController } from 'ionic-angular';
 /**
  * Generated class for the LibroPage page.
  *
@@ -18,75 +17,23 @@ import { AlertController } from 'ionic-angular';
 })
 export class LibroPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+	public lib: Libro;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	  this.lib = navParams.get("libroDado");
+	  alert(this.lib);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LibroPage');
   }
 
-  openPage(pagina)
-  {
+  openPage(pagina){
 	  this.navCtrl.push(pagina);
   }
   
-  irHome()
-  {
+  irHome(){
 	  this.navCtrl.setRoot(HomePage);
   }
   
-  disponible(){
-	  var biblio = document.getElementById("bibliovalue");
-	  var biblioteca = bibliovalue.options[bibliovalue.selectedIndex].value;
-	  if(biblioteca == "leg" || biblioteca == "gl" || biblioteca == "alc")
-	  {let disponible = false;}
-	  else
-	  {let disponible = true;}
-  
-	  return disponible;
-  }
-  
-  showAlert() {
-	 
-	  var biblio = document.getElementById("bibliovalue");
-	  var biblioteca = bibliovalue.options[bibliovalue.selectedIndex].value;
-	  
-	  if(biblioteca == "leg" || biblioteca == "gl" || biblioteca == "alc"){
-			this.navCtrl.setRoot(HomePage);
-	  
-			let alert = this.alertCtrl.create({
-			title: 'En espera',
-			subTitle: 'Te has puesto en la lista de espera. Se te avisará cuando este disponible',
-			buttons: ['OK']
-			});
-			alert.present();
-		}
-		
-	  else{
-		var alertfecha = document.getElementById("fecha").value;
-		if(alertfecha != ""){
-			this.navCtrl.setRoot(HomePage);
-			
-			let alert = this.alertCtrl.create({
-			title: 'Reservado',
-			subTitle: 'Puede recoger el libro en la fecha indicada: '+alertfecha,
-			buttons: ['OK']
-			});
-			alert.present();
-			
-		}
-			
-		else{
-		  let alert = this.alertCtrl.create({
-			title: 'Especifica una fecha',
-			subTitle: 'No se ha introducido una fecha valida',
-			buttons: ['OK']
-			});
-			alert.present();
-		}
-	  }
-	   
-    
-  }
-
 }
